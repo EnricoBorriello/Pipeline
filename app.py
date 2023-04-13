d1bc17520d1bb0d4762856d6ce05f7e9094171e9
Enrico Borriello <borriello.enrico@gmail.com>
2023-04-13 15:57:32 -0700

    Add files via upload

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ st.image(logo)
 # ---------------------
 
 
-st.header('Monthly Pipeline Review')
+
 #st.write('**contact:** enrico.borriello@asu.edu - **Latest update:** Apr 11, 2023')
 
 
@@ -49,9 +49,16 @@ features_columns = ['triad_'+("%02d" % (number,)) for number in range(1,14)]
 
 if uploaded_pre_file is not None and uploaded_post_file is not None:
 
+  st.header('Monthly Pipeline Review')
+
   df_pre = pd.read_csv(uploaded_pre_file)
   df_post = pd.read_csv(uploaded_post_file)
-    
+  
+  # replacing NaN with 0 
+  # making 'Annual hotel spend' int
+  df_pre['Annual hotel spend'] = df_pre['Annual hotel spend'].fillna(0).astype(int)
+  df_post['Annual hotel spend'] = df_post['Annual hotel spend'].fillna(0).astype(int)
+
   pre_owners = np.sort(np.array(df_pre['Opportunity Owner'].unique()))
   post_owners = np.sort(np.array(df_post['Opportunity Owner'].unique()))
 
@@ -67,8 +74,9 @@ if uploaded_pre_file is not None and uploaded_post_file is not None:
      'Annual hotel spend'  #,
      #'Account: Last Activity'
      ]].sort_values(by=['Annual hotel spend'], ascending=False
-      ).set_index('Account Name'
-      )#.style.set_precision(0)
+      ).rename(columns={'Account Name':'account','Annual hotel spend':'AHS'})
+      #.set_index('Account Name'
+      #).style.set_precision(0)
 
   accounts_post = df_post[df_post['Opportunity Owner']==owner][
     ['Account Name',
@@ -78,16 +86,50 @@ if uploaded_pre_file is not None and uploaded_post_file is not None:
      'Annual hotel spend' #,
      #'Account: Last Activity'
      ]].sort_values(by=['Annual hotel spend'], ascending=False
-      ).set_index('Account Name'
-      )#.style.set_precision(0)
+      ).rename(columns={'Account Name':'account','Annual hotel spend':'AHS'})
+      #.set_index('Account Name'
+      #).style.set_precision(0)
 
 
+  max_bars = 16
 
-  st.subheader('Prospects (Pre-FTB) View Summary')
-  st.dataframe(accounts_pre)
+  with st.expander('Prospects (Pre-FTB) View Summary'):
 
-  st.subheader('Customers (Post-FTB)')
-  st.dataframe(accounts_post)
+    st.subheader('Prospects (Pre-FTB) View Summary')
+    col1, col2 = st.columns([1,1])
+  
+    with col1:
+      df0 = df_pre[df_pre['Opportunity Owner']==owner]
+      df = df0.sort_values(by=['Annual hotel spend'],ascending = False).iloc[:max_bars]
+
+      c = alt.Chart(df).mark_bar().encode(
+        alt.X('Account Name',sort = None),
+        alt.Y('Annual hotel spend', axis=alt.Axis(grid=False))
+        ).properties(width=400,height=450).mark_bar(size=13)
+
+      st.altair_chart(c, use_container_width=True)
+
+    with col2:
+      st.dataframe(accounts_pre)
 
 
+  with st.expander('Customers (Post-FTB)'):
+
+    st.subheader('Customers (Post-FTB)')
+    col1, col2 = st.columns([1,1])
+  
+  
+    with col1:
+      df0 = df_post[df_post['Opportunity Owner']==owner]
+      df = df0.sort_values(by=['Annual hotel spend'],ascending = False).iloc[:max_bars]
+                     
+      c = alt.Chart(df).mark_bar().encode(
+        alt.X('Account Name',sort = None),
+        alt.Y('Annual hotel spend', axis=alt.Axis(grid=False))
+        ).properties(width=400,height=450).mark_bar(size=13)
+
+      st.altair_chart(c, use_container_width=True)
+
+    with col2:
+      st.dataframe(accounts_post)
 
