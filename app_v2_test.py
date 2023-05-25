6af436ecd82e852be93f68aeceeb7c266d24a313
Enrico Borriello <borriello.enrico@gmail.com>
2023-05-24 18:50:32 -0700

    Add files via upload

--- app_v2_test.py
+++ app_v2_test.py
@@ -148,9 +148,10 @@ if uploaded_pre_file is not None and uploaded_post_file is not None:
 
   df = df.set_index(pd.Index(index_names))
   #df.style.applymap(("{:,.0f}".format)
-  df.applymap('{:,.0f}'.format)
-
-  st.dataframe(df)
+  #df.applymap('{:,.0f}'.format)
+  df_formatted = df.round(0)
+  
+  st.dataframe(df_formatted)
 
 
 
