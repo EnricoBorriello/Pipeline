4f46361aa951fea1285f538498a451fa08093eb9
Enrico Borriello <borriello.enrico@gmail.com>
2023-05-24 18:25:02 -0700

    Add files via upload

--- app_v2_test.py
+++ app_v2_test.py
@@ -134,8 +134,8 @@ if uploaded_pre_file is not None and uploaded_post_file is not None:
 
 
   # visualization
-  df_total = df.loc['$ Total Pipeline']
-  df_expected = df.loc['$ Expected FTB Bookings']
+  #df_total = df.loc['$ Total Pipeline']
+  #df_expected = df.loc['$ Expected FTB Bookings']
   vialualize_total_pipeline (df_total,df_expected)
 
 
